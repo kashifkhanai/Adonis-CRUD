90a3ef7b9510716a9128d51812cb976b57881041
kashifkhanai <mkashifkhanai@gmail.com>
2025-08-26 18:14:24 +0500

    Add validotrs for user create or update

---
+++ app/validators/user.ts
@@ -0,0 +1,25 @@
+// validator for creating new user
+import vine from '@vinejs/vine'
+
+export const CreatUserValidator = vine.compile(
+  vine.object({
+    fullname: vine.string().trim().minLength(3).maxLength(255).nullable(),
+    email: vine
+      .string()
+      .email()
+      .unique(async (db, value) => {
+        const user = await db.from('users').where('email', value).first()
+        return !user
+      }),
+    password: vine.string().minLength(8).maxLength(180),
+  })
+)
+
+// validator for updating existing user
+export const UpdateUserValidator = vine.compile(
+  vine.object({
+    fullname: vine.string().trim().minLength(3).optional(),
+    email: vine.string().email().optional(),
+    password: vine.string().minLength(8).maxLength(180).optional(),
+  })
+)
