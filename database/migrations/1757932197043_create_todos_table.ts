13eb9a84a77f2f37ea30a379aeb40f4bea799b2b
kashifkhanai <mkashifkhanai@gmail.com>
2025-09-15 16:15:15 +0500

    Create migration for todos

---
+++ database/migrations/1757932197043_create_todos_table.ts
@@ -0,0 +1,24 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'todos'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title').notNullable()
+      table.boolean('is_completed').defaultTo(false)
+
+      // foreign key to users table
+      table.integer('user_id').unsigned().notNullable()
+      table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE')
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
