d393a46b740d7d9cbf846512230be014bf3de92e
kashifkhanai <mkashifkhanai@gmail.com>
2025-09-22 16:51:26 +0500

    add role column in user table

--- database/migrations/1758536366059_create_add_roles_table.ts
+++ database/migrations/1758536366059_create_add_roles_table.ts
@@ -5,6 +5,7 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.alterTable(this.tableName, (table) => {
+      //add role column to users table
       table.string('role').nullable().defaultTo('user')
     })
   }
